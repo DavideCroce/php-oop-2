42fa31f8384022bbe3a763c94397512da537e478
IskahnX <davidecroce10@gmail.com>
2022-03-04 17:19:53 +0100

    added no description function

--- Product.php
+++ Product.php
@@ -28,10 +28,16 @@ class Product
             $this->type = $type;
         }
     }
+    public function noDescription()
+    {
+        if ($this->description = '') {
+            return $this->description = 'No description';
+        }
+    }
 }
 
 $product1 = new Product('Royal Canin Maxi Adult', 52.90, 'https://arcaplanet.vtexassets.com/arquivos/ids/243809-1200-auto?v=637455010535500000&width=1200&height=auto&aspect=true', 126414894634484, 'Royal Canin', 'Food', 'Crocchette per cani Royal Canin Size Maxi Adult. Alimento secco completo per cani adulti di grande taglia, con un peso compreso tra i 26 e i 44 kg, dai 15 mesi ai 5 anni di età.', 2, 'Dog');
-$product2 = new Product('Tiragraffi Badalona Trixie Beige', 36.95, 'https://arcaplanet.vtexassets.com/arquivos/ids/223801-1200-auto?v=637454735937700000&width=1200&height=auto&aspect=true', 6491343254923752759, 'Trixie', 'Toy', 'Nessuna Descrizione', 1, 'Cat');
+$product2 = new Product('Tiragraffi Badalona Trixie Beige', 36.95, 'https://arcaplanet.vtexassets.com/arquivos/ids/223801-1200-auto?v=637454735937700000&width=1200&height=auto&aspect=true', 6491343254923752759, 'Trixie', 'Toy', '', 1, 'Cat');
 
 echo '<pre>';
 var_dump($product1);
