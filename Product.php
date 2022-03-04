5da16f0aba93ac9763f378e4add5e699d6cf011f
IskahnX <davidecroce10@gmail.com>
2022-03-04 15:58:01 +0100

    created some classes

---
+++ Product.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/Type.php';
+class Product
+{
+    protected $name;
+    protected $code;
+    protected $price;
+    protected $quantity;
+    protected $brand; //classe brand
+    use Type; //classe tipo
+    protected $description;
+    protected $picture;
+    protected $animal; // classe animals
+
+    public function __construct($name, $price, $picture, $code, $brand, $description, $quantity, $animal, $type)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->picture = $picture;
+        $this->code = $code;
+        $this->brand = $brand;
+        $this->type = $type;
+        $this->decription = $description;
+        $this->quantity = $quantity;
+        $this->animal = $animal;
+    }
+}
+
+$product1 = new Product('Royal Canin Maxi Adult', 52.90, 'https://arcaplanet.vtexassets.com/arquivos/ids/243809-1200-auto?v=637455010535500000&width=1200&height=auto&aspect=true', 126414894634484, 'Royal Canin', 'Crocchette per cani Royal Canin Size Maxi Adult. Alimento secco completo per cani adulti di grande taglia, con un peso compreso tra i 26 e i 44 kg, dai 15 mesi ai 5 anni di età.', 2, 'Dog', 'Food');
+$product2 = new Product('Tiragraffi Badalona Trixie Beige', 36.95, 'https://arcaplanet.vtexassets.com/arquivos/ids/223801-1200-auto?v=637454735937700000&width=1200&height=auto&aspect=true', 6491343254923752759, 'Trixie', 'Nessuna Descrizione', 1, 'Cat', 'Toy');
+
+echo '<pre>';
+var_dump($product1);
+var_dump($product2);
+echo '</pre>';
